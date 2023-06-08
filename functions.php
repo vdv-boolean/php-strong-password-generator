9470ed41c0bbce734cdd8c41ae4a7d3465229971
vincenzodivita <divitavincenzo@icloud.com>
2023-06-08 16:22:18 +0200

    Paste lenght var and function

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+    # Number of password character
+    $length = $_GET['length'];
+
+    # Function for generate random passord
+    function generatePassword($length) {
+        return substr(str_shuffle(str_repeat($all_character='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($all_character)) )),1,$length);
+    }
