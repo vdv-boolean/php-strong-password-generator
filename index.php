5b93c5e6abbbfe900c58c3a8cac431bfda5a72c5
vincenzodivita <divitavincenzo@icloud.com>
2023-06-08 15:44:42 +0200

    Add draft of html code

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password Generator</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Password Generator</title>
+    </head>
+    <body>
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+        <form action="" method="get">
+            <div>
+                <label for="lenght">Numero di caratteri:</label>
+                <input type="number" name="lenght" id="lenght"> 
+                button>Genera</button>
+            </div>
+            
+            <textarea name="" id="" cols="30" rows="1"></textarea>
+        </form>
+    </body>
 </html>
