4e29ad4bf827ad344702fe08347a4c73d80345b1
vincenzodivita <divitavincenzo@icloud.com>
2023-06-08 16:24:27 +0200

    Fix comment line

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
     # Number of password character
     $length = $_GET['length'];
 
-    # Function for generate random passord
+    # Function for generate random password
     function generatePassword($length) {
         return substr(str_shuffle(str_repeat($all_character='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($all_character)) )),1,$length);
     }
