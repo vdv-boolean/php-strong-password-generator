81c426e3dfda7e10e55f87a6c7c9ed0407cac21d
vincenzodivita <divitavincenzo@icloud.com>
2023-06-08 18:38:55 +0200

    Add simply style with bootstrap classes

--- index.php
+++ index.php
@@ -15,37 +15,42 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
     <body>
-        <!-- General titles -->
-        <h1>Strong Password Generator</h1>
-        <h2>Genera una password sicura</h2>
 
-        <?php if ($length == "") { ?>
+        <div class="container d-flex flex-column align-items-center m-4">
+             <!-- General titles -->
+            <h1>Strong Password Generator</h1>
+            <h2>Genera una password sicura</h2>
 
-            <div class="error-message">
-                <span><?=
-                    $error_message
-                ?></span>
-            </div>
+            <?php if ($length == "") { ?>
 
-        <?php } ?>
-        
+                <div class="error-message">
+                    <span><?=
+                        $error_message
+                    ?></span>
+                </div>
 
-        <!-- Form with GET method -->
-        <form action="" method="get">
-            <div>
-                <label for="length">Lunghezza password:</label>
-                <input type="number" min="6" name="length" id="length" value="<?= $length ?>"> 
-                <button>Genera</button>
-            </div>
+            <?php } ?>
             
-            <!-- Text area for print generated password -->
-            <textarea  
-                rows="1"
-                cols="<?= $length ?>"
-            ><?php 
-                if ($length != "") {
-                    echo generatePassword($length);
-                } ?></textarea>
-        </form>
+
+            <!-- Form with GET method -->
+            <form action="" method="get" class="d-flex flex-column align-items-center m-4">
+                <div>
+                    <label for="length">Lunghezza password:</label>
+                    <input type="number" min="6" name="length" id="length" value="<?= $length ?>"> 
+                    <button class="btn btn-dark">Genera</button>
+                </div>
+                
+                <!-- Text area for print generated password -->
+                <textarea  
+                    rows="1"
+                    cols="<?= $length ?>"
+                ><?php 
+                    if ($length != "") {
+                        echo generatePassword($length);
+                    } 
+                ?></textarea>
+            </form>
+        </div>
+
     </body>
 </html>
