5d9ecc927a33594915d0b66c26fb0f6be21d4382
vincenzodivita <divitavincenzo@icloud.com>
2023-06-08 19:08:04 +0200

    Add advanced password generator function

--- functions.php
+++ functions.php
@@ -7,6 +7,14 @@
     $length = $_GET['length'];
 
     # Function for generate random password
-    function generatePassword($length) {
-        return substr(str_shuffle(str_repeat($all_character='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($all_character)) )),1,$length);
-    }
+    // function generatePassword($length) {
+    //     return substr(str_shuffle(str_repeat($all_character='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($all_character)) )),1,$length);
+    // }
+
+    # Function for generate advanced random password
+    function generateAdvancedPassword($length) {
+        $caps_character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $number_character = '0123456789';
+        $symbol_character = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+        return substr(str_shuffle(str_repeat($merge_character=$caps_character . $number_character . $symbol_character . 'abcdefghijklmnopqrstuvwxyz', ceil($length/strlen($merge_character)) )),1,$length);
+    }   
